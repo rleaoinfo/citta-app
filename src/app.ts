3360ba12de257aeffd420db672946f4796662f24
rleaoinfo <rleaoinfo@gmail.com>
2021-02-18 20:19:54 -0300

    adjust on search cities

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import bodyParser from "body-parser";
 import connect from "../config/connect";
+import * as city_controller from "./controller/city_controller"
 
 dotenv.config();
 
@@ -12,6 +13,12 @@ const PORT = 3000;
 app.get('/', (req, res) => res.send('Server is ON'));
 
 connect(mongo_url);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/city/allcities", city_controller.allCities)
+app.post("/city/addcity" , city_controller.addCity);
 
 app.listen(PORT, () => {
   console.log(`Server Running on https://localhost:${PORT}`);
--- src/model/citymodel.ts
+++ src/model/citymodel.ts
@@ -1,24 +1,23 @@
-import mongoose, { Schema, Document } from 'mongoose';
+import mongoose, { Schema, Document } from "mongoose";
 
 export interface ICity extends Document {
-    name: string;
-    uf: string;
-    area: number;
-    population: number;
-    active: boolean;
-    CreatedAt: Date;
-    updatedAt: Date;
-  }
-
+  name: string;
+  uf: string;
+  area: number;
+  population: number;
+  active: boolean;
+  CreatedAt: Date;
+  updatedAt: Date;
+}
 
 const CitySchema: Schema = new Schema({
   name: { type: String, required: true },
   uf: { type: String, required: true },
   area: { type: Number, required: true },
   population: { type: Number, required: true },
-  active: { type: Boolean, required: true },
-  CreatedAt: { type: Date, required: true },
-  updatedAt: { type: Date, required: true }
+  active: { type: Boolean, required: true, default: true },
+  CreatedAt: { type: Date, required: true, default: new Date() },
+  updatedAt: { type: Date, required: true, default: new Date() },
 });
 
-export default mongoose.model<ICity>('City', CitySchema);
+export default mongoose.model<ICity>("City", CitySchema);
