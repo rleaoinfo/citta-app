89f857654ae8f5c1acab0c7f7a41ece7583a8ba0
rleaoinfo <rleaoinfo@gmail.com>
2021-02-23 17:35:13 -0300

    adjust on errors

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,45 +1,67 @@
 export const ErrorName = (name: any) => {
-  if (typeof name === "string") {
-    const nameLenght = name.length;
-    if (nameLenght < 3) {
-      return { value: false, description: "Name less than 3 characters" };
-    } else if (nameLenght > 250) {
-      return { value: false, description: "Name more than 250 characters" };
-    } else {
-      return { value: true, description: "Ok" };
-    }
+  if (name == null || name == undefined || name == "") {
+    return { value: false, description: "Name is null , not defined or blank" };
   } else {
-    return { value: false, description: "Name is not a string" };
+    if (typeof name === "string") {
+      const nameLenght = name.length;
+      if (nameLenght < 3) {
+        return { value: false, description: "Name less than 3 characters" };
+      } else if (nameLenght > 250) {
+        return { value: false, description: "Name more than 250 characters" };
+      } else {
+        return { value: true, description: "Ok" };
+      }
+    } else {
+      return { value: false, description: "Name is not a string" };
+    }
   }
 };
 
 export const ErrorUf = (uf: any) => {
-  const letters = /^[A-Za-z]+$/
-  if (typeof uf === "string" && uf.match(letters)) {
-    const ufLength = uf.length;
-    if (ufLength < 2 || ufLength > 2) {
-      return { value: false, description: "Uf must have 2 characters" };
-    } else {
-      return { value: true, description: "Ok" };
-    }
+  if (uf == null || uf == undefined || uf == "") {
+    return { value: false, description: "uf is null , not defined or blank" };
   } else {
-    return { value: false, description: "Uf is not a string or have numbers" };
+    const letters = /^[A-Za-z]+$/;
+    if (typeof uf === "string" && uf.match(letters)) {
+      const ufLength = uf.length;
+      if (ufLength < 2 || ufLength > 2) {
+        return { value: false, description: "Uf must have 2 characters" };
+      } else {
+        return { value: true, description: "Ok" };
+      }
+    } else {
+      return {
+        value: false,
+        description: "Uf is not a string or have numbers",
+      };
+    }
   }
 };
 
 export const ErrorArea = (Area: any) => {
-  if (typeof Area === "number") {
-    return { value: true, description: "Ok" };
+  if (Area == null || Area == undefined || Area == "") {
+    return { value: false, description: "Area is null , not defined or blank" };
   } else {
-    return { value: false, description: "Area must be a number" };
+    if (typeof Area === "number") {
+      return { value: true, description: "Ok" };
+    } else {
+      return { value: false, description: "Area must be a number" };
+    }
   }
 };
 
 export const ErrorPopulation = (Population: any) => {
-  if (typeof Population === "number") {
-    return { value: true, description: "Ok" };
+  if (Population == null || Population == undefined || Population == "") {
+    return {
+      value: false,
+      description: "Population is null , not defined or blank",
+    };
   } else {
-    return { value: false, description: "Population must be a number" };
+    if (typeof Population === "number") {
+      return { value: true, description: "Ok" };
+    } else {
+      return { value: false, description: "Population must be a number" };
+    }
   }
 };
 
